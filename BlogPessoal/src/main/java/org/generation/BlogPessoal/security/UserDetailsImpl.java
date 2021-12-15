1ac3c58b11e4590a89742861eb518198e51563c6
Matheus Gomes <mgrspeed@gmail.com>
2021-12-15 19:48:38 -0300

    @Rorschach616 [Task: Security implement]

---
+++ BlogPessoal/src/main/java/org/generation/BlogPessoal/security/UserDetailsImpl.java
@@ -0,0 +1,62 @@
+package org.generation.BlogPessoal.security;
+
+import java.util.Collection;
+
+import org.generation.BlogPessoal.Model.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImpl implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	private String userName;
+	private String password;
+
+	public UserDetailsImpl(Usuario user) {
+		this.userName = userName;
+		this.password = password;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return userName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
